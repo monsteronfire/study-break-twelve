b63056abbc4534295de8cf4021673733f8214384
monsteronfire <monsteronfire@gmail.com>
2017-03-01 17:43:32 +0800

    Add link+user associations
    * Also add destroy method
    * Also show link
    * Also list links on index

--- raddit/app/controllers/links_controller.rb
+++ raddit/app/controllers/links_controller.rb
@@ -1,28 +1,49 @@
 class LinksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @links = Link.all
   end
 
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
+
+    if @link.save
+      redirect_to @link
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
+  def update
+    if @link.update(link_params)
+      redirect_to @link
+    else
+      render 'edit'
+    end
+  end
+
   def edit
   end
 
-  def delete
+  def destroy
+    @link.destroy
+    redirect_to root_path
   end
 
   private
+  def set_link
+    @link = Link.find(params[:id])
+  end
+
   def link_params
     params.require(:link).permit(:title, :url)
   end
