d390db1203a2ab917aaee8ad1d6f0ba960d77530
monsteronfire <monsteronfire@gmail.com>
2017-03-03 21:32:41 +0800

    Add ingredient and direction model
    * Also add association between ingredients, directions and recipes
    * Add nav parials
    * Add recipe controller actions
    * Add recipe views

--- recipe/app/controllers/recipes_controller.rb
+++ recipe/app/controllers/recipes_controller.rb
@@ -1,2 +1,30 @@
 class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_recipe
+  end
+
+  def recipe_params
+  end
 end
