5c5852621830347491375d5f5865c2004dc98587
monsteronfire <monsteronfire@gmail.com>
2017-03-03 23:30:27 +0800

    Follow cocoon html structure to a T
    * Debug forms
    * Show ingredients and directios on recipe show page

--- recipe/app/models/recipe.rb
+++ recipe/app/models/recipe.rb
@@ -5,5 +5,5 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
   mount_uploader :picture, PictureUploader
-  validates :title, :description, :image, presence: true
+  validates :title, :description, :picture, presence: true
 end
