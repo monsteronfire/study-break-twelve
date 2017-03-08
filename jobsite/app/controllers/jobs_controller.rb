d03d4cd7957f89b49339b5787b7c27ba9709105c
monsteronfire <monsteronfire@gmail.com>
2017-03-08 16:50:14 +0800

    Add job category filter

--- jobsite/app/controllers/jobs_controller.rb
+++ jobsite/app/controllers/jobs_controller.rb
@@ -3,7 +3,12 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
-    @jobs = Job.all
+    if params[:category].blank?
+      @jobs = Job.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
