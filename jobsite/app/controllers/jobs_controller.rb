8f45ffe64c2e2eb1f84d15e23a1db290cf52b9ed
monsteronfire <monsteronfire@gmail.com>
2017-03-08 16:28:00 +0800

    Add user and job

--- jobsite/app/controllers/jobs_controller.rb
+++ jobsite/app/controllers/jobs_controller.rb
@@ -1,4 +1,51 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [ :show, :edit, :update, :destroy ]
+  before_action :authenticate_user!, except: [ :index, :show ]
+
   def index
+    @jobs = Job.all
+  end
+
+  def show
+  end
+
+  def new
+    @job = current_user.jobs.build
+  end
+
+  def create
+    @job = current_user.jobs.build(job_params)
+
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update
+      redirect_to @job
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description, :company, :url, :user_id)
   end
 end
